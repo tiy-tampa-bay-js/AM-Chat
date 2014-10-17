4e6db4dcb48a798f522c469b38682d692c76d5d3
Alyssa <alyssa@thepalesnail.com>
2014-10-17 12:12:19 -0400

    taking a well deserved break

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 var template = _.template($('#chat').html()); // grab the template
 var apiUrl   = 'http://tiny-pizza-server.herokuapp.com/collections/AM-Chat';
+var messageObject = {}; // Create me an empty object
 //Our api url
 var button = document.getElementById('button');
 
@@ -13,32 +14,37 @@ button.onclick = function() {
     }
 };
 
-
-$('.message-input input[type=submit]').on('click', function (event) {
-  //Whenever the user clicks the create button in the form
-
+$('#formPop input[type=submit]').on('click', function (event) {
   event.preventDefault(); //Dont let the browser submit the form
 
-  var fieldValues = $('input.field').serializeArray();
+  var loginValues =  $('input.userName').serializeArray();
 
   //Turn everything into an array of js objects and not DOM objects
 
-  var messageObject = {}; // Create me an empty object
-
-
-  fieldValues.forEach(function (field) {
-    messageObject[field.name] = field.value;
+  loginValues.forEach(function (userName) {
+    messageObject[userName.name] = userName.value;
   });
-  //Fill my form object with the name:value pair of each form field.
 
-  $.ajax({
-    method: 'POST',
-    url: apiUrl,
-    data: messageObject
-    //Send a POST request to our API to save the data from the form.
-  }).done(function (data) { $('input.field').val('') });
-  //Clear all the data in the form after its done.
-});
+
+  $('.message-input input[type=submit]').on('click', function (event) {
+    //Whenever the user clicks the create button in the form
+
+    event.preventDefault(); //Dont let the browser submit the form
+
+    var fieldValues = $('input.field').serializeArray();
+
+    fieldValues.forEach(function (field) {
+      messageObject[field.name] = field.value;
+    });
+    //Fill my form object with the name:value pair of each form field.
+    $.ajax({
+      method: 'POST',
+      url: apiUrl,
+      data: messageObject
+      //Send a POST request to our API to save the data from the form.
+    }).done(function (data) { $('input.field').val('') });
+    //Clear all the data in the form after its done.
+  });
 
 
 var previousCount = 0;
@@ -52,6 +58,12 @@ setInterval(function () {
         previousCount = allTheChats.length;
 
         var finishedTemplates = _.map(allTheChats, function (chat) {
+          if (_.isUndefined(chat.message)) {
+            chat.message = " "
+          }
+          if (_.isUndefined(chat.user)){
+            chat.user = " "
+          }
           return template(chat);
         });
 
@@ -64,3 +76,4 @@ setInterval(function () {
    })
 }, 1000);
 //Everyone 1s run this anonymous function
+});
