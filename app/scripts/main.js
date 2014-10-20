f77e51ef0711c28b5dd44c4557ef2eadb6b39e8a
Alyssa <alyssa@thepalesnail.com>
2014-10-20 15:04:08 -0400

    mountains

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,48 +17,48 @@ button.onclick = function() {
     }
 };
 
-$('#formPop input[type=submit]').on('click', function (event) {
-  event.preventDefault(); //Dont let the browser submit the form
-
-  var loginValues =  $('input.userName').serializeArray();
-
-  //Turn everything into an array of js objects and not DOM objects
-
-  loginValues.forEach(function (userName) {
-    messageObject[userName.name] = userName.value;
-  });
-
-
-  $('.message-input input[type=submit]').on('click', function (event) {
-    //Whenever the user clicks the create button in the form
-
+  $('#formPop input[type=submit]').on('click', function (event) {
     event.preventDefault(); //Dont let the browser submit the form
-    var sentAt = new Date();
-    messageObject['sentAt'] = sentAt;
 
-    var fieldValues = $('input.field').serializeArray();
-    fieldValues.forEach(function (field) {
-      messageObject[field.name] = field.value;
+    var loginValues =  $('input.userName').serializeArray();
+
+    //Turn everything into an array of js objects and not DOM objects
+
+    loginValues.forEach(function (userName) {
+      messageObject[userName.name] = userName.value;
     });
 
 
-      console.log(messageObject);
-    //Fill my form object with the name:value pair of each form field.
-    $.ajax({
-      method: 'POST',
-      url: apiUrl,
-      data: messageObject
-      //Send a POST request to our API to save the data from the form.
-    }).done(function (data) { $('input.field').val('') });
-    //Clear all the data in the form after its done.
-  }); // Closes line 21
+    $('.message-input input[type=submit]').on('click', function (event) {
+      //Whenever the user clicks the create button in the form
+
+      event.preventDefault(); //Dont let the browser submit the form
+      var sentAt = new Date();
+      messageObject['sentAt'] = sentAt;
+
+      var fieldValues = $('input.field').serializeArray();
+      fieldValues.forEach(function (field) {
+        messageObject[field.name] = field.value;
+      });
 
 
-var previousCount = 0;
+        console.log(messageObject);
+      //Fill my form object with the name:value pair of each form field.
+      $.ajax({
+        method: 'POST',
+        url: apiUrl,
+        data: messageObject
+        //Send a POST request to our API to save the data from the form.
+      }).done(function (data) { $('input.field').val('') });
+      //Clear all the data in the form after its done.
+      });
 
-setInterval(function () {
-  $.ajax({url: apiUrl}).done(function (allTheChats) {
-  // Do a GET request on our API to return all previously saved TODOs
+
+  var previousCount = 0;
+
+  var messageList = function () {
+    $.ajax({url: apiUrl}).done(function (allTheChats) {
+
      if(allTheChats.length > previousCount) {
        console.log(allTheChats);
 
@@ -81,20 +81,24 @@ setInterval(function () {
 
       }// closes if all the chats.length statement
    }) // ajax call end for allthechats
+  };// end of messageList
 
-   $.ajax({url: apiUrl}).done(function (allThePersons) {
+  var peopleList = function () {
+    $.ajax({url: apiUrl}).done(function (allThePersons) {
 
-         var finishedSideTemplates = _.map(allThePersons, function (person) {
-           if (_.isUndefined(person.user)){
-             person.user = " "
-           }
-           return sideTemplate(person);
-         });
+        var finishedSideTemplates = _.map(allThePersons, function (person) {
+          if (_.isUndefined(person.user)){
+            person.user = " "
+          }
+          return sideTemplate(person);
+        });
+        $('#persons').html(finishedSideTemplates);
+     });//closes ajax for allthepersons
+   };// end function peopleList
 
-         $('#persons').html(finishedSideTemplates);
-
-  });//closes ajax for allthepersons
-
-}, 1000); // end of set interval line 60
+  setInterval(function () {
+    peopleList();
+    messageList();
+  }, 1000); // end of set interval line 60
 
  }); //superend from line 21
